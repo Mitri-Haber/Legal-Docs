4aae956efa79a656e8e532c11045b1a46f49ba6b
Mitri <haber.mitri0@gmail.com>
2024-09-04 21:42:46 +0300

    cleanup and comment

--- DocScrapper/DocScrapper/settings.py
+++ DocScrapper/DocScrapper/settings.py
@@ -8,15 +8,6 @@ import os
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
-
-user = os.getenv("MONGODB_USER")
-password = os.getenv("MONGODB_PASSWORD")
-# pwd = urllib.parse.quote_plus(password) # in case the password needs the be parsed
-host = os.getenv("MONGODB_HOST")
-MONGO_URI = f"mongodb://{user}:{password}@{host}:27017/?authMechanism=DEFAULT"
-MONGO_DATABASE = os.getenv("MONGODB_DATABASE")
-
-
 BOT_NAME = "DocScrapper"
 
 SPIDER_MODULES = ["DocScrapper.spiders"]
@@ -25,22 +16,21 @@ NEWSPIDER_MODULE = "DocScrapper.spiders"
 LOG_FILE = 'crawler.log'
 LOG_LEVEL = 'INFO'
 
+#Mongo connection string and DAtabase to be used
+MONGO_URI = f'mongodb://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@{os.getenv("MONGODB_HOST")}:27017/?authMechanism=DEFAULT'
+MONGO_DATABASE = os.getenv("MONGODB_DATABASE")
+
+#Azure blob connection string
+AZURE_ACCOUNT_URL = f"https://{os.getenv('AZURE_ACCOUNT_NAME')}.blob.core.windows.net/"
+AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
+
 ROBOTSTXT_OBEY = True
 DOWNLOAD_HANDLERS = {
 "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
 }
-
-TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-PLAYWRIGHT_BROWSER_TYPE = "chromium"
-REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
-FEED_EXPORT_ENCODING = "utf-8"
  
- 
-AZURE_ACCOUNT_URL = f"https://{os.getenv('AZURE_ACCOUNT_NAME')}.blob.core.windows.net/"
-AZURE_ACCOUNT_KEY = os.getenv("AZURE_ACCOUNT_KEY")
-
-
+#Mongo db pipline to handle upserts
 ITEM_PIPELINES = {
    "DocScrapper.pipelines.MongoDBPipeline": 300,
  
@@ -49,3 +39,7 @@ ITEM_PIPELINES = {
 DOWNLOADER_MIDDLEWARES = {
    "DocScrapper.middlewares.DocscrapperSpiderMiddleware": 543,
 }
+
+TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
+REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
+FEED_EXPORT_ENCODING = "utf-8"
--- DocScrapper/DocScrapper/spiders/LexFindSystematic.py
+++ DocScrapper/DocScrapper/spiders/LexFindSystematic.py
@@ -7,6 +7,12 @@ from Connections.AzureDataLake import AzureStorageManager
 from hashlib import sha256
 from DocScrapper.items import PdfMetaData
 import json
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+START_URL = 'https://www.lexfind.ch/api/fe/de/global/systematics'
 
 WEB_CONFIG = json.load(open('DocScrapper\lex_find_web_config.json'))
 
@@ -16,25 +22,28 @@ HEADERS = WEB_CONFIG['headers']
 
 PDF_HEADERS = WEB_CONFIG['pdf_headers']
 
-MAPPER = json.loads(requests.get('https://www.lexfind.ch/api/fe/de/global/systematics', params= INIT_PARAMS , headers= HEADERS).content)
-
-LANGUAGES = ['de', 'it', 'fr']
+LANGUAGES = ['it', 'de', 'fr']
 
 BASE_URL = "https://www.lexfind.ch/api/fe/de/global/systematics?active_only=false&category_filter%5B%5D=1&category_filter%5B%5D=2&category_filter%5B%5D=3&category_filter%5B%5D=4&category_filter%5B%5D=5&category_filter%5B%5D=6&category_filter%5B%5D=7&category_filter%5B%5D=8&category_filter%5B%5D=9&entity_filter%5B%5D=1&entity_filter%5B%5D=10&entity_filter%5B%5D=11&entity_filter%5B%5D=12&entity_filter%5B%5D=13&entity_filter%5B%5D=14&entity_filter%5B%5D=15&entity_filter%5B%5D=16&entity_filter%5B%5D=17&entity_filter%5B%5D=18&entity_filter%5B%5D=19&entity_filter%5B%5D=2&entity_filter%5B%5D=20&entity_filter%5B%5D=21&entity_filter%5B%5D=22&entity_filter%5B%5D=23&entity_filter%5B%5D=24&entity_filter%5B%5D=25&entity_filter%5B%5D=26&entity_filter%5B%5D=27&entity_filter%5B%5D=28&entity_filter%5B%5D=3&entity_filter%5B%5D=4&entity_filter%5B%5D=5&entity_filter%5B%5D=6&entity_filter%5B%5D=7&entity_filter%5B%5D=8&entity_filter%5B%5D=9&tols_for_systematics%5B%5D={tols_id}"
+
+try:
+    MAPPER =  requests.get(START_URL , params= INIT_PARAMS , headers= HEADERS).content
+except requests.exceptions.RequestException as e:
+    logger.error(f"Request failed at initial MAPPERS: {e}")
+
+MAPPER = json.loads(MAPPER)
 
 class LexfindsystematicSpider(scrapy.Spider):
     name = "LexFindSystematic"
     allowed_domains = ["www.lexfind.ch"]
 
     def __init__(self):
-
-
+        
         self.blob_storage_manager = AzureStorageManager()
-
         super(LexfindsystematicSpider, self).__init__()
 
     def start_requests(self):
- 
+        """Perform intial requests on the tol ids that are in the initial MAPPER"""
         for key in MAPPER.keys() :
              
             yield JsonRequest(
@@ -49,7 +58,9 @@ class LexfindsystematicSpider(scrapy.Spider):
             )
 
     def parse_page(self, response, tols_id):
- 
+        """From the tols in the response iterate on the pattern to create the link where we can find the download link, this is for metadata only
+           As well as the version url which will be used to get the id for the download link for the latest pdf"""
+        
         response = response.json()[tols_id]
         tols = response['tols']
 
@@ -68,33 +79,43 @@ class LexfindsystematicSpider(scrapy.Spider):
                         }
                 yield Request(
                     version_url,
-                    callback=self.parse,
+                    callback=self.enrich_meta_and_parse_pdf,
                     cb_kwargs={
-                    "tols_id" : tols_id,
+                 
                     "meta_data": meta_data
                 } 
                 )
 
-    def parse(self, response, tols_id, meta_data) -> PdfMetaData:
-        item =PdfMetaData()
+    def enrich_meta_and_parse_pdf(self, response, meta_data) -> PdfMetaData:
+        """This processing function retreives additional metada,
+            creates the pdf download link,
+            download and upload the pdf,
+            and upserts metadata to MONGODB"""
+        
+        #reteive latest version of pdf, to get dates, and id
         version_meta_data = response.json()['families'][0][0][0]
         meta_data['version_initial_date'] = version_meta_data['version_active_since']
         meta_data['version_last_update'] = version_meta_data['version_active_since']
-         
+        
+        #get id from latest version , and create the download link
         latest_pdf_sys_id = version_meta_data['id']
         pdf_download_url = f"https://www.lexfind.ch/tolv/{latest_pdf_sys_id}/{meta_data['language']}"
         meta_data['pdf_download_url'] = pdf_download_url
 
+        #download and hash the pdf
         pdf_binary = requests.get(pdf_download_url, headers= PDF_HEADERS).content
         meta_data['pdf_content_hash'] = sha256(pdf_binary).hexdigest()
 
+        #get month year in order to upload to a corresponding folder
         _, month, year = meta_data['version_last_update'].split('.')
 
+        # create upload link, and upload pdf
         blob_storage_relative_path = f"{meta_data['language']}/{year}/{month}/{meta_data['pdf_content_hash']}.pdf"
         meta_data['blob_relative_path'] = blob_storage_relative_path
         self.blob_storage_manager.upload_data(pdf_binary, blob_storage_relative_path)
         meta_data['ts_inserted'] = datetime.now(timezone.utc)
-
+        
+        # copy metadata to PdfMetadata and yield it.
         item =PdfMetaData()
         item.update(meta_data)
         yield item
